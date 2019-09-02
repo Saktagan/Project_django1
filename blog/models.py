17dfb5c9b76b82540328515a9cfebe3df2532a08
Saktagan <saktagan0013@gmail.com>
2019-09-02 19:52:16 +0600

    new interface project:
    :

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
+from django.db import models
+
+# Create your models here.
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
-
-# Create your models here.
 
 
 class Post(models.Model):
@@ -18,4 +18,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return "{} --- {}".format(self.title, self.author)
+        return "{0}---{1}".format(self.author, self.title)
